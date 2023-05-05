706cff0fff2b986116ad4d36956c4f021e10ee67
Gabriel Lovric <gabriel.lovric@solyze.com>
2023-05-05 09:40:39 +0200

    implement basic functionality

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,70 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      availableKeysets: [
+        [['q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'ü', ' '], 'Oberereihe'],
+        [['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ö', 'ä', ' '], 'Grundreihe'],
+        [['y', 'x', 'c', 'v', 'b', 'n', 'm', ' '], 'Unterereihe']
+      ],
+      keyset: null,
+      textLength: 100,
+      pointer: 0,
+      text: null
+    }
+  },
+  methods: {
+    start() {
+      this.pointer = 0;
+      this.text = Array.from(
+        { length: this.textLength },
+        () => this.keyset[Math.floor(Math.random() * this.keyset.length)]);
+
+      window.addEventListener('keyup', this.type)
+    },
+    type(event) {
+      console.log(event)
+      if (event.key === this.text[this.pointer]) {
+        this.pointer++;
+      }
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div v-if="!text">
+      <select v-model="keyset">
+        <option
+          v-for="([key, value]) in availableKeysets"
+          :value="key"
+        >
+          {{ value }}
+        </option>
+      </select>
+
+      <select v-model="textLength">
+        <option>100</option>
+        <option>500</option>
+        <option>1000</option>
+      </select>
+
+      <button
+        v-if="keyset"
+        @click="start"
+      >Start</button>
+    </div>
+
+    <div
+      v-if="text"
+      class="text"
+    >
+      <span
+        v-for="(key, i) in text"
+        :class="{ active: pointer === i }"
+      >{{ key }}</span>
+    </div>
+
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
